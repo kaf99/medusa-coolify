094c9ee811831536c5d6e39a2f7bef1b74961e47
nexthis <pawel.romanowski000@gmail.com>
2025-04-30 22:24:06 +0200

    update docker

--- medusa/migrations.sh
+++ medusa/migrations.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$WORKER_MODE" != "worker" ]; then
-  echo "Running migrations..."
-  medusa migrations run
+  echo "Running migrations for $WORKER_MODE"
+  yarn run predeploy 
 else
   echo "Skipping migrations because WORKER_MODE=worker"
 fi
